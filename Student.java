f9cce8e24005d218453b3fef1db530b6ed16d242
ShaikHaseenaBegum <141468063+ShaikHaseenaBegum@users.noreply.github.com>
2023-08-20 23:13:06 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,24 @@
+package internsavy_task4;
+
+	import java.util.ArrayList;
+	import java.util.List;
+	import java.util.Scanner;
+
+	class Student {
+	    private int id;
+	    private String name;
+
+	    public Student(int id, String name) {
+	        this.id = id;
+	        this.name = name;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+	
+}
